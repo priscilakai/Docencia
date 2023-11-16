a07cfd49272f0f5f943fcc43d025944bfe9697e0
Priscila M Kai <43282604+priscilakai@users.noreply.github.com>
2023-11-16 18:01:57 -0400

    Add files via upload

---
+++ AEDII/Aula 2/aula2_fila_solucao.c
@@ -0,0 +1,62 @@
+/*
+Neste arquivo, apenas as funções enqueue() e dequeue() são apresentadas
+Faça uma solução completa para simular uma fila de inteiros. Faça uso de laços para observar o comportamento FIFO
+*/
+
+#include <stdio.h>
+#define MAX 100
+
+/* as funções enqueue e dequeue armazenarão as posições para os números adicionados. */
+char queue[MAX];
+// spos e rpos são variáveis globais
+int spos = 0; //contém o índice da próxima posição de armazenamento livre
+int rpos = 0; //contém o índice do próxima evento a ser recuperado
+
+/*Armazena um inteiro*/
+void enqueue(int item)
+{
+    if(spos==MAX)
+    {
+        printf("Lista de eventos cheia!\n");
+        return;
+    }
+    else
+    {
+        printf("Item %d adicionado com sucesso!\n",item);
+    }
+    queue[spos] = item;
+    spos++;
+}
+
+/*Recupera um evento*/
+int dequeue()
+{
+    int item;
+    if(rpos==spos)
+    {
+        printf("Sem eventos.\n");
+        return -1;
+    }
+    item = queue[rpos];
+    rpos++;
+    return item;
+
+}
+
+int main()
+{
+    int i;
+    for(i=0;i<=10;i++){
+        enqueue(i);
+    }
+    int item = dequeue();
+    if (item != -1) {
+        while (item != -1){
+            printf("%d foi desenfileirado.\n", item);
+            item = dequeue();
+        }
+    }
+    return 0;
+
+}
+
